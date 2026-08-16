19b94759b99a23f87101fbd75c158e13312af3cb
subhash817 <vsubhash817@gmail.com>
2026-08-16 07:53:05 +0530

    grater number

---
+++ 01_basics/04_greater_number.py
@@ -0,0 +1,23 @@
+num1=int(input("Enter first number : "))
+num2= int(input("Enter second number: "))
+
+if num1 > num2:
+    print(num1, "is grater")
+elif num2 > num1:
+    print(num2, "is grater")
+else:
+    print("Both numbers are equal")   
+
+'''
+Enter first number : 12
+Enter second number: 13
+13 is grater
+
+Enter first number : 14
+Enter second number: 10
+14 is grater
+
+Enter first number : 10   
+Enter second number: 10
+Both numbers are equal
+'''         
---
+++ 01_basics/05_largest_of_three.py
@@ -0,0 +1,4 @@
+a=10
+b=50
+c=30
+print(f'{a > b and a > c}')
