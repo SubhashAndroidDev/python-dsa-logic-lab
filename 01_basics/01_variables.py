0ff4d75d2a25b06925cc95c3a12ea7584835316d
subhash817 <vsubhash817@gmail.com>
2026-08-16 07:39:25 +0530

    odd even program

---
+++ 01_basics/01_variables.py
@@ -0,0 +1,10 @@
+'''
+A variable has a name, and you can store something in it.
+
+Variables can hold different types of data, like whole numbers, decimal numbers, or text.
+
+
+'''
+favAnimal = 'turtles'
+
+print('I like ' + favAnimal)
---
+++ 01_basics/02_even_odd.py
@@ -0,0 +1,15 @@
+number=int(input("Enter a number: "))
+# check whether the number is divisible by 2
+if(number %2==0):
+    print("even")
+else:
+    print("odd") 
+     
+
+'''
+    Enter a number: 21
+    odd
+
+    Enter a number: 10
+    even
+'''  
