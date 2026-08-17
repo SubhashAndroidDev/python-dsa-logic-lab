4391f268c0f8f4503ec306b0545527cf03314e1c
subhash817 <vsubhash817@gmail.com>
2026-08-17 07:18:51 +0530

    String topic

---
+++ 03_strings/AssignStringVariable.py
@@ -0,0 +1,7 @@
+"""
+Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
+
+
+"""
+name="Subhahs Verma"
+print(f"{name}")
---
+++ 03_strings/QuotesInsideQuotes.py
@@ -0,0 +1,7 @@
+"""
+You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
+
+"""
+print("It's alright")
+print("he is called 'Subhash Verma'")
+print('He is called "Subhash" ')
