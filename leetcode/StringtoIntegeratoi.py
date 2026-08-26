00b5856bdefc5058ea5e9a224c6637de76b9e4c2
Subhash Verma <vsubhash817@gmail.com>
2026-08-26 22:52:55 +0530

    String to Integer atoi program

--- leetcode/StringtoIntegeratoi.py
+++ leetcode/StringtoIntegeratoi.py
@@ -1,50 +1,56 @@
 class Solution:
-    def myAtoi(self,s:str) -> int:
-        i=0
-        n=len(s)
-        # 32 bit integer limits
-        INT_MIN=-2**31
-        INT_MAX=2**31 - 1
-        #STEP 1: skip leading space
-        while i< n and s[i]== '':
-            i+=1
+    def myAtoi(self, s: str) -> int:
 
-            # step2 check sign
-            
-            sign=1
-            if i < n and s[i] == '-':
-                sign=-1
-                i+=1
-            elif i<n and s[i] == '+':
-                i+=1
+        i = 0
+        n = len(s)
 
-            # step 3: convert digits
-            num=0
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
 
-            while i < n and s[i].isdigit():
-                digit=int(s[i])
+        # Step 1: Skip leading spaces
+        while i < n and s[i] == ' ':
+            i += 1
 
-                num=num * 10 + digit
+        # Step 2: Check sign
+        sign = 1
 
-                i += 1
+        if i < n and s[i] == '-':
+            sign = -1
+            i += 1
 
-            # apply sign
-            num=num*sign
+        elif i < n and s[i] == '+':
+            i += 1
 
-            if num  < INT_MIN:
-                return INT_MIN
+        # Step 3: Read digits
+        num = 0
 
-            if num > INT_MAX:
-                return INT_MAX
+        while i < n and s[i].isdigit():
 
-            return num
+            digit = int(s[i])
 
+            num = num * 10 + digit
+
+            i += 1
+
+        # Apply sign
+        num = num * sign
+
+        # Step 4: Check 32-bit range
+        if num < INT_MIN:
+            return INT_MIN
+
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
+
+
+# Create object
 solution = Solution()
 
+# Test cases
 print(solution.myAtoi("42"))
 print(solution.myAtoi(" -042"))
 print(solution.myAtoi("1337c0d3"))
 print(solution.myAtoi("0-1"))
-print(solution.myAtoi("words and 987"))        
-
-
+print(solution.myAtoi("words and 987"))
