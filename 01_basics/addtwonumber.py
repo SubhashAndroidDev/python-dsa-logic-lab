5373d4c7f0cced684426f002bcc85aefd45ef4f6
Subhash Verma <vsubhash817@gmail.com>
2026-08-29 15:22:13 +0530

    sum of two number using user input

---
+++ 01_basics/addtwonumber.py
@@ -0,0 +1,10 @@
+
+"""Python Program to Add Two Numbers"""
+num1=10
+num2=20
+print("the sum of two numbers is:", num1+num2)
+
+number1=float(input("Enter first number: "))
+number2=float(input("Enter second number: "))
+sum=number1+number2
+print("the sum of {0} and {1} is {2}".format(number1,number2,sum))
