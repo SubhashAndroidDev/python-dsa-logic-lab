f5d2db364991a506fb28393299a9089c6d83474c
Subhash Verma <vsubhash817@gmail.com>
2026-08-30 14:57:31 +0530

    swap two numbers without using third variable

--- 01_basics/SquareRoot.py
+++ 01_basics/SquareRoot.py
@@ -1,4 +1,12 @@
+import math
+# solution 1
 num =64
 num1=int(input("Enter a number: "))
 sr=num1 ** 0.5
 print("The square root of", num1, "is", sr)
+
+
+# solution 2
+num=int(input("Enter a number: "))
+sr=math.sqrt(num)
+print("The square root of", num, "is", sr)
---
+++ 01_basics/SwapTwoVariables.py
@@ -0,0 +1,15 @@
+x=12
+y=13
+temp = x
+print("the value of temp variable is:", temp)
+
+x=y
+print("the value of x variable is:", x)
+y=temp
+print("the value of y variable is:", y)
+print("================================")
+
+# swapping two variables without using third variable
+x,y = y, x
+print("the value of x variable is:", x)
+print("the value of y variable is:", y)
