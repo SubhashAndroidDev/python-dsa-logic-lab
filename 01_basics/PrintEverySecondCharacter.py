049bf553dec9ecffeba2b73165e8d826c4082655
Subhash Verma <vsubhash817@gmail.com>
2026-09-01 23:19:41 +0530

    slicing on string

---
+++ 01_basics/PrintEverySecondCharacter.py
@@ -0,0 +1,20 @@
+text = "abcdefghij"
+
+result = text[::2]
+
+print(result)
+
+"""
+dry run 
+
+text  = a b c d e f g h i j
+index = 0 1 2 3 4 5 6 7 8 9
+
+text[::2]
+
+0 → a
+2 → c
+4 → e
+6 → g
+8 → i
+"""
---
+++ 01_basics/RemoveFirstLastCharacter.py
@@ -0,0 +1,18 @@
+text = "Python"
+
+result = text[1:-1]
+
+print(result)
+
+
+"""
+text = P y t h o n
+index  0 1 2 3 4 5
+
+text[1:-1]
+
+Start → index 1 = y
+End   → index -1 (n), not included
+
+Result = "ytho"
+"""
